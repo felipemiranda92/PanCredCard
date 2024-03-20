ab29f0457b25cf1d938e7417190b805ea90f3ae6
Felipe Miranda Santos <felipe_miranda92@outlook.com>
2024-03-20 20:52:32 -0300

    Features de acessibilidade

--- PanCredCard/PanCredCard/DetailsScreen/View/DetailsViewController.swift
+++ PanCredCard/PanCredCard/DetailsScreen/View/DetailsViewController.swift
@@ -38,15 +38,28 @@ class DetailsViewController: UIViewController {
     func setupDetails() {
         
         credCardNameLabel.text = creditCard?.name
-        credCardAliasLabel.text = creditCard?.alias
+        credCardNameLabel.text = creditCard?.alias
+        credCardNameLabel.isAccessibilityElement = true
+        credCardNameLabel.accessibilityHint = "Esta é a bandeira do cartão selecionado"
+        
         
         if let creditCard = creditCard {
             creditLabel.text = viewModel.configureCreditLabel(creditCard: creditCard)
+            creditLabel.isAccessibilityElement = true
+            creditLabel.accessibilityHint = "Essa é a função de crédito"
+            
             credCardDebitLabel.text = viewModel.configureDebitLabel(creditCard: creditCard)
+            credCardDebitLabel.isAccessibilityElement = true
+            credCardDebitLabel.accessibilityHint = "Essa é a função de débito"
         }
         
         credCardNumberLabel.text = creditCard?.number
+        credCardNumberLabel.isAccessibilityElement = true
+        credCardNumberLabel.accessibilityHint = "Esse é o número do cartão"
+        
         credCardCodSecLabel.text = creditCard?.codSEC
+        credCardCodSecLabel.isAccessibilityElement = true
+        credCardCodSecLabel.accessibilityHint = "Esse é o código do cartão"
     }
 }
 
